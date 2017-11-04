dff2153a629a834051383fca2939e242218c5bbd
29984608 <29984608@qq.com>
2017-11-04 19:41:54 +0800

    hibernate测试

---
+++ src/main/java/Utils/HibernateUtils.java
@@ -0,0 +1,29 @@
+package Utils;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by pc on 2017/9/22.
+ */
+public class HibernateUtils {
+    private static Configuration cfg = null;
+    private static SessionFactory sessionFactory = null;
+    static {
+        cfg = new Configuration();
+        cfg.configure();
+        sessionFactory = cfg.buildSessionFactory();
+    }
+    //提供返回与本地线程绑定的session的方法
+    public static Session getSessionObject(){
+        return sessionFactory.getCurrentSession();
+    }
+    public  SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+    public static void main(String [] args){
+
+    }
+
+}
---
+++ src/main/java/entity/User.java
@@ -0,0 +1,43 @@
+package entity;
+
+/**
+ * Created by yang on 17-11-4.
+ */
+public class User {
+    private int id;
+    private String username;
+    private int password;
+    private int salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
